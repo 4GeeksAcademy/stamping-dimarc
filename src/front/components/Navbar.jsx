d4f3c2408abc4836737615f357276b99b09a259a
Sindy93 <sindyyorlene@gmail.com>
2025-08-19 01:08:36 +0000

    Agregué el navbar con logo y buscador

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -3,16 +3,31 @@ import { Link } from "react-router-dom";
 export const Navbar = () => {
 
 	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
+		<nav className="navbar" style={{ backgroundColor: "#907ab6" }}>
+			<div className="container d-flex justify-content-between align-items-center">
+
+				{/* LOGO */}
 				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+					<img 
+						src="https://raw.githubusercontent.com/4GeeksAcademy/stamping-dimarc/refs/heads/main/src/front/assets/img/logo-horizontal-stamping.png" 
+						alt="Stamping Dimarc" 
+						style={{ height: "50px" }} 
+					/>
 				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
+
+				{/* BUSCADOR */}
+				<form className="d-flex" role="search">
+					<input 
+						className="form-control me-2" 
+						type="search" 
+						placeholder="Buscar..." 
+						aria-label="Search" 
+					/>
+					<button className="btn btn-outline-light" type="submit">
+						<i className="fas fa-search"></i>
+					</button>
+				</form>
+
 			</div>
 		</nav>
 	);
