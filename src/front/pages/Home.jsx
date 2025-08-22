4c8eecb24a56e6e4092be8d93ba021e25cfd0659
Samuel Carmona <samuel.carmona.rodrigz@gmail.com>
2025-08-22 21:20:23 +0000

    :art: Add ClosingBanner component to enhance the homepage design

--- src/front/pages/Home.jsx
+++ src/front/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "../components/Hero";
+import ClosingBanner from "../components/ClosingBanner";
 
 export const Home = () => {
   return (
     <>
       <Hero />
+      <ClosingBanner />
     </>
   );
 };
